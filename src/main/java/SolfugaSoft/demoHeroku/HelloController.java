a47130e8bd190c0480df0b1a907cd7f19309cb61
Bartłomiej Głowacz <glowaczbrt@gmail.com>
2019-02-16 01:43:41 +0100

    aplication.properties

--- src/main/java/SolfugaSoft/demoHeroku/HelloController.java
+++ src/main/java/SolfugaSoft/demoHeroku/HelloController.java
@@ -1,7 +1,7 @@
 package SolfugaSoft.demoHeroku;
 
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
